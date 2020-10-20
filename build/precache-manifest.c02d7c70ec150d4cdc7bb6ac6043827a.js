0bbe622b20adf0f0bd4e4fb15a2542c30ecff38a
duckhue01 <duckhuejs@gmail.com>
2020-10-20 10:40:55 +0700

    add bundle file

---
+++ build/precache-manifest.c02d7c70ec150d4cdc7bb6ac6043827a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "057e89e47832c86965edc5d7d4c190ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3255e8f7844403aef8ee",
+    "url": "/static/css/main.b1f87d30.chunk.css"
+  },
+  {
+    "revision": "eef4770b1eb89cdee54a",
+    "url": "/static/js/2.1fe46c20.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.1fe46c20.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3255e8f7844403aef8ee",
+    "url": "/static/js/main.4e042277.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
